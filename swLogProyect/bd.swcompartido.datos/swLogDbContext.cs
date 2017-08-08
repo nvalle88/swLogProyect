6bcf572f206b95d0b29e91efae7e25ff3fb434c1
Paúl Cevallos <pcevallos@digitalstrategy.com.ec>
2017-08-08 13:10:25 -0500

    prueba

--- swLogProyect/bd.swcompartido.datos/swLogDbContext.cs
+++ swLogProyect/bd.swcompartido.datos/swLogDbContext.cs
@@ -17,6 +17,8 @@ namespace bd.swLogProyect.datos
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
+
+
             base.OnModelCreating(builder);
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
